0ffa7daf51861eb99f9824085d07eafd4ae07b9f
corvo007 <oscarlongsslz@gmail.com>
2023-06-23 23:05:11 +0800

    add const.py

---
+++ const.py
@@ -0,0 +1,27 @@
+song_rating_coefficient = [
+    [0, 0, "d"],
+    [50, 8, "c"],
+    [60, 9.6, "b"],
+    [70, 11.2, "bb"],
+    [75, 12.0, "bbb"],
+    [80, 13.6, "a"],
+    [90, 15.2, "aa"],
+    [94, 16.8, "aaa"],
+    [97, 20, "s"],
+    [98, 20.3, "sp"],
+    [99, 20.8, "ss"],
+    [99.5, 21.1, "ssp"],
+    [99.9999, 21.4, "ssp"],
+    [100, 21.6, "sss"],
+    [100.4999, 22.2, "sss"],
+    [100.5, 22.4, "sssp"],
+]
+VERSION_FILE = "https://bucket-1256206908.cos.ap-shanghai.myqcloud.com/update.json"
+STAT_API = "https://www.diving-fish.com/api/maimaidxprober/chart_stats"
+
+DX_CHART = 0
+STD_CHART = 1
+
+LIKE = 0
+DISLIKE = 1
+
