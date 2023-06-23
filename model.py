9818264100d5bac03032a804aea64aa8f4aad1fe
corvo007 <oscarlongsslz@gmail.com>
2023-06-23 23:06:35 +0800

    update dataclass definition

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
-from typing import Any, Dict, List, Literal, Optional, Union
+from typing import Dict, List, Literal, Optional
 
 from pydantic import BaseModel, Field, IPvAnyAddress, root_validator, validator
+
+from const import *
 
 
 class DataBaseConfigModel(BaseModel):
@@ -27,41 +29,10 @@ class player_preferences(BaseModel):
     recommend_preferences: Literal["aggressive", "balance", "conservative"] = "balance"
     exclude_played: bool = False
 
-
-class RecommendSongsModel(BaseModel):
+class BasicChartInfoModel(BaseModel):
     song_id: int = None
     level: int = None
-    chart_design: str = None
-    tap_note: int = None
-    hold_note: int = None
-    slide_note: int = None
-    touch_note: int = None
-    break_note: int = None
-    difficulty: float = None
-    old_difficulty: float = None
-
-    __data__: Dict[str, Any] = None
-    _dirty: List[str] = None
-    __rel__: Dict[str, Any] = None
-    sample_num: int
-    fit_difficulty: float
-    avg_achievement: float
-    avg_dxscore: float
-    std_dev: float
-    achievement_dist: List[int]
-    fc_dist: List[float]
-    like: int
-    dislike: int
-    weight: float
-    artist: str
-    song_title: str
-    bpm: int
-    version: str
-    genre: str
-    is_new: bool
-    type: int
-    vote: Optional[int]
-
+    
     @root_validator(pre=True)
     def move_data_to_root(cls, values):
         data = values.pop("__data__", {})
@@ -70,6 +41,16 @@ class RecommendSongsModel(BaseModel):
         values.pop("__rel__", None)
         return values
 
-    @validator("achievement_dist", "fc_dist", pre=True)
-    def convert_string_to_list(cls, value):
-        return [float(num) for num in value.strip("[]").split(",")]
+
+class RecommendChartsModel(BasicChartInfoModel):
+    vote: Optional[Literal[LIKE, DISLIKE, None]] = None
+
+
+class DiffStatData(BaseModel):
+    achievements: float
+    dist: List[float]
+    fc_dist: List[float]
+
+
+class AllDiffStatData(BaseModel):
+    diff_data: Dict[str, DiffStatData]
