0b46a9397950bc7d7fa85ce159889594eb12250d
corvo007 <oscarlongsslz@gmail.com>
2023-07-07 15:14:25 +0800

    add exception class

--- exception.py
+++ exception.py
@@ -8,5 +8,16 @@ class Error(Exception):
         return f"遇到了错误：\n{self.message}"
 
 
-class ParameterError(Exception):
-    pass
+class ParameterError(Error):
+    def __init__(self, message: str = "参数错误"):
+        self.message = message
+
+
+class NoSuchPlayerError(Error):
+    def __init__(self, message: str = "无此玩家，请检查输入"):
+        self.message = message
+
+
+class InvaildTokenError(Error):
+    def __init__(self, message: str = "凭证无效"):
+        self.message = message
